75d3cb05bd73a4a7ee0d3eaa8829a11d06a71546
Muhammad Umer <mumer1297@gmail.com>
2020-09-20 17:26:13 +0500

    [feat] navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,25 +2,43 @@ import React from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
 import logo from "../images/logo.svg";
+import shoppingBag from "../images/shoppingBag.svg";
 
 export const Navbar = () => {
   return (
-    <div>
-      <Link to="/" className={"link"}>
-        <img src={logo} alt="logo" />
-      </Link>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/" className={"link"}>
+              <img src={logo} alt="logo" />
+            </Link>
+          </li>
 
-      <Link to="/" className={"link"}>
-        Men
-      </Link>
+          <li>
+            <Link to="/" className={"link"}>
+              Men
+            </Link>
+          </li>
 
-      <Link to="/products" className={"link"}>
-        Women
-      </Link>
-
-      <Link to="/about" className={"link"}>
-        Kids
-      </Link>
-    </div>
+          <li>
+            <Link to="/products" className={"link"}>
+              Women
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className={"link"}>
+              Kids
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div class="rightSec">
+        <input type="text" placeholder="Search" />
+        <Link to="/shoppingCart" className={"link"}>
+          <img src={shoppingBag} alt="shopping cart" />
+        </Link>
+      </div>
+    </header>
   );
 };
