cc6fc128825e46862a5a88567caa2431693c5f29
Muhammad Umer <mumer1297@gmail.com>
2020-09-20 17:31:41 +0500

    Updates

---
+++ precache-manifest.451fdca34c22e26fb972338dea0016f6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fea9934885cb0f97e54e1226e38b468",
+    "url": "/react-shoe-store/index.html"
+  },
+  {
+    "revision": "272e05a8d2dc13536f0b",
+    "url": "/react-shoe-store/static/css/main.920870e4.chunk.css"
+  },
+  {
+    "revision": "1abdc34b6a9aab5ec6e0",
+    "url": "/react-shoe-store/static/js/2.f25479dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-shoe-store/static/js/2.f25479dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "272e05a8d2dc13536f0b",
+    "url": "/react-shoe-store/static/js/main.8a18d785.chunk.js"
+  },
+  {
+    "revision": "5010b4c0f1ea972acb80",
+    "url": "/react-shoe-store/static/js/runtime-main.16bf714b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-shoe-store/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cfa4680f7b0b705ea5ec533b273c8b0c",
+    "url": "/react-shoe-store/static/media/logo.cfa4680f.svg"
+  },
+  {
+    "revision": "ba4d0d00f7195b56f11bcae2a986cd5f",
+    "url": "/react-shoe-store/static/media/shoppingBag.ba4d0d00.svg"
+  }
+]);
