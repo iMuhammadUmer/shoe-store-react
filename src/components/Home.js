94e3f44ba388ff7173cf16b714f9cbb3804b8a62
Muhammad Umer <mumer1297@gmail.com>
2020-09-21 09:54:30 +0500

    [feat] grid

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
 import React from "react";
+import "../App.css";
 
 export const Home = () => {
   return (
     <div>
-      <h1>Welcome to Home</h1>
+      <div class="grid-container">
+        <div class="grid-item">1</div>
+        <div class="grid-item">2</div>
+        <div class="grid-item">3</div>
+        <div class="grid-item">4</div>
+        <div class="grid-item">5</div>
+        <div class="grid-item">6</div>
+        <div class="grid-item">7</div>
+        <div class="grid-item">8</div>
+        <div class="grid-item">9</div>
+      </div>
     </div>
   );
 };
