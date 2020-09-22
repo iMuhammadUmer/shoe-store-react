5c0b05ba32cf2f3df49c21279e29fdf8dfa9348e
Muhammad Umer <mumer1297@gmail.com>
2020-09-22 22:57:33 +0500

    [update] name and prices

--- src/components/ShoeDescription.js
+++ src/components/ShoeDescription.js
@@ -5,7 +5,11 @@ import shoeImg from "../images/men-shoe1.png";
 export const ShoeDescription = () => {
   return (
     <div class="showDescription">
-      <div class="col">Show discription and price</div>
+      <div class="col">
+        <h1 id="shoeName">nike react</h1>
+        <p>description</p>
+        <p id="price">$499</p>
+      </div>
       <img src={shoeImg} alt="shoeimage" />
       <div class="col">shoe images</div>
     </div>
