f4667f16a33408271cf081ff77b34d80c2751566
Muhammad Umer <mumer1297@gmail.com>
2020-09-22 13:43:28 +0500

    [update] routes updated for development

--- src/Routes.js
+++ src/Routes.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Home } from "./components/Home.js";
 import { Men } from "./components/Men.js";
 import { Women } from "./components/Women.js";
-import { Kids } from "./components/Kids.js";
+// import { Kids } from "./components/Kids.js";
+import { ShoeDescription } from "./components/ShoeDescription.js";
 import { Navbar } from "./components/Navbar";
 import { ShoppingCart } from "./components/ShoppingCart";
 
@@ -17,7 +18,7 @@ function RouteConfig() {
           <Route exact path="/" component={Home} />
           <Route exact path="/men" component={Men} />
           <Route exact path="/women" component={Women} />
-          <Route exact path="/kids" component={Kids} />
+          <Route exact path="/shoe-description" component={ShoeDescription} />
           <Route exact path="/shopping-cart" component={ShoppingCart} />
           <Route path="*" component={() => <h2>Error 404</h2>} />
         </Switch>
