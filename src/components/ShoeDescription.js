92bd6bf3d21a1a2a3c2b577127d55037c8464530
Muhammad Umer <mumer1297@gmail.com>
2020-09-22 16:24:23 +0500

    [feat] basic structure

--- src/components/ShoeDescription.js
+++ src/components/ShoeDescription.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "../App.css";
+import shoeImg from "../images/men-shoe1.png";
 
 export const ShoeDescription = () => {
   return (
     <div class="showDescription">
-      <div class="col">asd</div>
+      <div class="col">Show discription and price</div>
+      <img src={shoeImg} alt="shoeimage" />
+      <div class="col">shoe images</div>
     </div>
   );
 };
