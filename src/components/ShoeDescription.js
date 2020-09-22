a0fc6c0f1ac1a87a326f30567125ee9d707bcc16
Muhammad Umer <mumer1297@gmail.com>
2020-09-22 13:44:26 +0500

    [WIP] shoeDescription

---
+++ src/components/ShoeDescription.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../App.css";
+
+export const ShoeDescription = () => {
+  return (
+    <div class="showDescription">
+      <div class="col">asd</div>
+    </div>
+  );
+};
