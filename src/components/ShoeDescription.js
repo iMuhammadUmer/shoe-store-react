986c84cb0076386b14c458ebbf371457b7b9128a
Muhammad Umer <mumer1297@gmail.com>
2020-09-26 23:25:21 +0500

    [feat] useParam for dynamic routing

--- src/components/ShoeDescription.js
+++ src/components/ShoeDescription.js
@@ -1,17 +1,24 @@
 import React from "react";
 import "../App.css";
-import shoeImg from "../images/men-shoe1.png";
+import { useParams } from "react-router-dom";
+import Shoes from "./../data.json";
 
 export const ShoeDescription = () => {
+  const { id } = useParams();
+  const shoe = Shoes[id];
+
+  if (!shoe) {
+    return <h2>Product not found</h2>;
+  }
   return (
     <div class="showDescription">
       <div class="col">
-        <h1 id="shoeName">nike react</h1>
-        <p>description</p>
-        <p id="price">$499</p>
+        <h1 id="shoeName">{shoe.name}</h1>
+        <p>{shoe.description}</p>
+        <p id="price">${shoe.price}</p>
       </div>
-      <img src={shoeImg} alt="shoeimage" />
-      <div class="col">shoe images</div>
+      <img src={shoe.img} alt="shoeimage" />
+      {/* <div class="col">shoe images</div> */}
     </div>
   );
 };
