f58d1b022b3562ecccc1538e6a626be719f25bef
Muhammad Umer <mumer1297@gmail.com>
2020-09-27 15:14:23 +0500

    [feat] col adjustments

--- src/components/ShoeDescription.js
+++ src/components/ShoeDescription.js
@@ -2,23 +2,27 @@ import React from "react";
 import "../App.css";
 import { useParams } from "react-router-dom";
 import Shoes from "./../data.json";
+// import pageNotFound from ".././images/pageNotFound.svg";
 
 export const ShoeDescription = () => {
   const { id } = useParams();
   const shoe = Shoes[id];
 
   if (!shoe) {
-    return <h2>Product not found</h2>;
+    return <h1>page not found</h1>;
+    // return <img src={pageNotFound} alt="error404" />;
   }
   return (
-    <div class="showDescription">
-      <div class="col">
-        <h1 id="shoeName">{shoe.name}</h1>
-        <p>{shoe.description}</p>
-        <p id="price">${shoe.price}</p>
+    <body>
+      <div class="showDescription">
+        <div class="col">
+          <h1 id="shoeName">{shoe.name}</h1>
+          <p>{shoe.description}</p>
+          <p id="price">${shoe.price}</p>
+        </div>
+        <img src={shoe.img} alt="shoeimage" />
+        {/* <div class="col">shoe images</div> */}
       </div>
-      <img src={shoe.img} alt="shoeimage" />
-      {/* <div class="col">shoe images</div> */}
-    </div>
+    </body>
   );
 };
