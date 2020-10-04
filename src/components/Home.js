b712b2fdf33d67828d60eafda75ae6e687b03a7a
Muhammad Umer <mumer1297@gmail.com>
2020-10-04 17:21:29 +0500

    [WIP] fetching image from JSON

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
-import Shoes from "./../data.json";
+import Shoes from "../data";
 
 export const Home = () => {
   return (
